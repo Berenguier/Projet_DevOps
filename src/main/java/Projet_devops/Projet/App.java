e89a3286cfed52af905ddec89ca37750e98dd910
Berenguier <lucas.berenguier@etu.univ-grenoble-alpes.fr>
2023-04-05 10:43:00 +0200

    constructueur tableau

--- src/main/java/Projet_devops/Projet/App.java
+++ src/main/java/Projet_devops/Projet/App.java
@@ -2,6 +2,9 @@ package Projet_devops.Projet;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+	Object[] arr1 ={1,2,3};
+	Object[] arr2 = {4,5,6};
+    Dataframe test = new Dataframe(arr1,arr2);
+    test.print();
   }
 }
---
+++ src/main/java/Projet_devops/Projet/Dataframe.java
@@ -0,0 +1,24 @@
+package Projet_devops.Projet;
+
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.util.Arrays;
+
+public class Dataframe{
+	Object[][] tab;
+	Dataframe(Object[]...args){
+		tab = args;
+	}
+	Dataframe(FileInputStream f){}
+	public void print() {
+		int n = tab[0].length;
+		for (int j=0;j<n;j++) {
+			for (int i =0;i<tab.length;i++) {
+				System.out.print(tab[i][j]);
+				System.out.print(" ");
+		}
+			System.out.println("");
+		}
+		
+	}
+}
