9d5530d0dbaf57c4f18d554d982b835c0198359d
zaghdar <bouchra.zaghdar@etu.univ-grenoble-alpes.fr>
2023-04-19 11:12:22 +0200

    constructeur qui pren un fichier en parametre

--- src/main/java/Projet_devops/Projet/App.java
+++ src/main/java/Projet_devops/Projet/App.java
@@ -1,13 +1,17 @@
 package Projet_devops.Projet;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
 public class App {
-  public static void main(String[] args) {
-	Object[] tab1 = {1,2,3};
-	Object[] tab2 = {"test","test2",null};
-	Object[] tab3 = {4,5,6};
-	Object []tab4 = {7.5,8.5,9.5};
-	Object []tab5 = {true,true,false};
-    Dataframe test = new Dataframe(tab1,tab2,tab3,tab4,tab5);
+  public static void main(String[] args) throws IOException {
+	  /*Object[] tab1 = {1,2,3};
+	    Object[] tab2 = {"test","test2",null};
+	    Object[] tab3 = {4,5,6};
+	    Object []tab4 = {7.5,8.5,9.5};
+	    Object []tab5 = {true,true,false};
+	    Dataframe test = new Dataframe(tab1,tab2,tab3,tab4,tab5);*/
+	Dataframe test = new Dataframe("src/main/java/Projet_devops/Projet/fichierVide.csv",',', StandardCharsets.UTF_8);
     test.print();
   }
 }
--- src/main/java/Projet_devops/Projet/Dataframe.java
+++ src/main/java/Projet_devops/Projet/Dataframe.java
@@ -1,17 +1,33 @@
 package Projet_devops.Projet;
-
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class Dataframe{
 	Object[][] tab;
+	boolean fichier = false;
 	Dataframe(Object[]...args){
 		tab = args;
 	}
-	Dataframe(FileInputStream f){}
+	
+	Dataframe(String f , char c , Charset charset) throws IOException{
+		fichier= true;
+		tab = Files.lines(Paths.get(f),charset)
+			    .map(ligne-> ligne.split(String.valueOf(c)))
+			    .toArray(String[][]::new);  
+		
+	}
 	public void print() {
 		int n = tab[0].length;
+		if(!fichier ) {
 		for (int j=0;j<n;j++) {
 			for (int i =0;i<tab.length;i++) {
 				System.out.print(tab[i][j]);
@@ -19,7 +35,16 @@ public class Dataframe{
 		}
 			System.out.println("");
 		}
-		
+		}
+		else {
+		for (int i=1; i<tab.length; i++) {
+			for (int j=0;j<n;j++) {
+			 System.out.print(tab[i][j]);
+			 System.out.print(" ");
+		}
+			System.out.println(" ");
+		}
+		}
 	}
 	public Object[] getColonne(int i) {
 		return tab[i];
