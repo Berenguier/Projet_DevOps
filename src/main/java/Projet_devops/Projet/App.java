88cc7c9b2cbf0ea0d5ff6115c7b0590d034654a0
zaghdar <bouchra.zaghdar@etu.univ-grenoble-alpes.fr>
2023-04-26 09:29:20 +0200

    Revert "constructeur"
    This reverts commit 93a686ffbcd79cdb3d9cb9a9874843af12f6ba81.

--- src/main/java/Projet_devops/Projet/App.java
+++ src/main/java/Projet_devops/Projet/App.java
@@ -1,24 +1,13 @@
 package Projet_devops.Projet;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-
 public class App {
-	 public static void main(String[] args) throws IOException {
-		    Object[] tab1 = {"label1",1,2,3};
-		    Object[] tab2 = {"label2","test","test2",null};
-		    Object[] tab3 = {"label3",4,5,6};
-		    Object []tab4 = {"label4",7.5,8.5,9.5};
-		    Object []tab5 = {"label5",true,true,false};
-		   //Dataframe test = new Dataframe(tab1,tab2,tab3,tab4,tab5);
-		    //int[] rowIndexes = {1, 3}; 
-		  //Dataframe selectedDataframe = test.selectLignes(rowIndexes);
-		 //   selectedDataframe.print();
-		Dataframe test = new Dataframe("src/main/java/Projet_devops/Projet/fichier.csv",',', StandardCharsets.UTF_8);
-		//Dataframe selectedDataframe = test.select_colonne_label("Sexe","Prénom");
-		//selectedDataframe.print();
-		test.print();
-		 Dataframe select = test.select_colonne_label("Sexe");
-		 select.print();
-	  }
+  public static void main(String[] args) {
+	Object[] tab1 = {1,2,3};
+	Object[] tab2 = {"test","test2",null};
+	Object[] tab3 = {4,5,6};
+	Object []tab4 = {7.5,8.5,9.5};
+	Object []tab5 = {true,true,false};
+    Dataframe test = new Dataframe(tab1,tab2,tab3,tab4,tab5);
+    test.print();
+  }
 }
--- src/main/java/Projet_devops/Projet/Dataframe.java
+++ src/main/java/Projet_devops/Projet/Dataframe.java
@@ -1,126 +1,27 @@
 package Projet_devops.Projet;
 
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.nio.charset.Charset;
 import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 
 public class Dataframe{
 	Object[][] tab;
-	Object[] label ;
 	Dataframe(Object[]...args){
 		tab = args;
-		label = new Object[tab.length];
-		for (int j = 0;j<tab.length;j++) {
-            label[j] = tab[j][0];
-        }
 	}
-
-	Dataframe(String f , char c , Charset charset) throws IOException{
-		Object[][] temp;
-		temp = Files.lines(Paths.get(f),charset)
-			    .map(ligne-> ligne.split(String.valueOf(c)))
-			    .toArray(String[][]::new); 
-		tab = new Object[temp[0].length][temp.length];
-		 for (int i =0;i<temp.length;i++) {
-	        	for (int j = 0;j<temp[0].length;j++) {
-	        		tab[j][i]=temp[i][j];
-	        	}
-	        }
-		 label = new Object[tab.length];
-		 for (int j = 0;j<tab.length;j++) {
-	            label[j] = tab[j][0];
-	        }
-	}
+	Dataframe(FileInputStream f){}
 	public void print() {
 		int n = tab[0].length;
 		for (int j=0;j<n;j++) {
 			for (int i =0;i<tab.length;i++) {
-				
 				System.out.print(tab[i][j]);
 				System.out.print(" ");
 		}
 			System.out.println("");
 		}
-
-	}
-	public void afficher_premiere_lignes() {
-		for (int j=0;j<tab.length;j++) {
-				System.out.print(tab[j][0]);
-				System.out.print(" ");
-		}
-			System.out.println("");
-	
-	}
-	public void afficher_derniere_lignes() {
-			for (int j=0;j<tab.length-1;j++){
-				System.out.print(tab[j][tab[0].length-1]);
-				System.out.print(" ");
-		}
-			System.out.println("");
-		}
-
 		
-	public Dataframe selectLignes(int...args) {
-		 Object[][] liste  = new Object[args.length+1][tab.length];
-		 Object[][] liste2  = new Object[tab.length][args.length+1];
-		 liste[0]= label;
-	        for (int i =0;i<args.length;i++) {
-	            liste[i+1] = this.getLigne(args[i]);    
-	        }
-	        for (int i =0;i<args.length+1;i++) {
-	        	for (int j = 0;j<tab.length;j++) {
-	        		liste2[j][i]=liste[i][j];
-	        	}
-	        }
-	        Dataframe select = new Dataframe(liste2);
-	        return select;  
-    }
-	public Dataframe selectColonne(int...args) {
-        Object[][] liste  = new Object[args.length][tab[0].length];
-        for (int i =0;i<args.length;i++) {
-            liste[i] = this.getColonne(args[i]);
-        }
-        Dataframe select = new Dataframe(liste);
-        return select;
-    }
-	public Dataframe select_colonne_label(String...args) {
-		 int[] indexes = new int[args.length];
-		    int idx = 0;
-		    for (int i = 0; i < tab[0].length; i++) {
-		        for (String label : args) {
-		            if (tab[0][i].equals(label)) {
-		                indexes[idx++] = i;
-		            }
-		        }
-		    }
-		    if (idx < args.length) {
-		        indexes = Arrays.copyOf(indexes, idx);
-		    }
-		    if (indexes.length!=0) {
-		    		return selectColonne(indexes);	
-		    }
-		    else {
-		    	System.out.println("label non valid");
-		    	return null;
-		    }
-    }
-	public Object[] getLigne(int i) {
-        Object[] ligne = new Object[tab.length];
-        for (int j = 0;j<tab.length;j++) {
-            ligne[j] = tab[j][i];
-        }
-        return ligne;
-    }
+	}
 	public Object[] getColonne(int i) {
 		return tab[i];
-	}	
+	}
 }
